af1946866163a858a6940e5fbe40ee98120bd146
Yerbanga Assetou Housnia <sawadogoastride220@gmail.com>
2026-06-27 17:14:55 +0000

    Sawadogo wendlassida Halenta Astride

---
+++ Etudiant.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+public class Etudiant{
+    private String nom;
+    private String prenom;
+    private ArrayList<Double> notes;
+    
+    public Etudiant (String nom, String prenom){
+        this.nom = nom;
+        this.prenom = prenom;
+        this.notes = new ArrayList<>();
+    }
+
+    public String attribuerMention(){
+        double moyenne = 12.0;
+        if (moyenne >= 16) return "Très Bien";
+        else if (moyenne >= 14) return "Bien";
+        else if (moyenne >=12) return "Assez Bien";
+        else if (moyenne >=10) return "Passable";
+        else return "Ajourné"
+        
+    }
+}
